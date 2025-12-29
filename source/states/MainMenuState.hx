40cd4d7efab71cc850deb6ae4db8468a579e9a80
StarNova <creambr6@gmail.com>
2025-12-29 13:46:45 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -108,6 +108,11 @@ class MainMenuState extends MusicBeatState
 		#end
 		#end
 
+		#if mobile
+		addVirtualPad(UP_DOWN, A_B);
+		addVirtualPadCamera();
+		#end
+
 		super.create();
 
 		FlxG.camera.follow(camFollow, null, 9);

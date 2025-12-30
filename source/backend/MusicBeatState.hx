684df0f582f5907f9b548812bf1b5d8fbae43b7c
StarNova <creambr6@gmail.com>
2025-12-30 20:37:00 -0300

    Só um teste

--- source/backend/MusicBeatState.hx
+++ source/backend/MusicBeatState.hx
@@ -56,11 +56,11 @@ class MusicBeatState extends FlxUIState
 	{
 		mobileHitbox = new MobileHitbox();
 
-		camControls = new FlxCamera();
+		/*camControls = new FlxCamera();
 		camControls.bgColor.alpha = 0;
 		FlxG.cameras.add(camControls, false);
 
-		mobileHitbox.cameras = [camControls];
+		mobileHitbox.cameras = [camControls];*/
 		mobileHitbox.visible = false;
 		add(mobileHitbox);
 	}

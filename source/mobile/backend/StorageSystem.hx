a99abe7e56f6b599f72955b8af7a5073243e714f
StarNovaBR <creambr6@gmail.com>
2026-01-23 17:25:56 -0300

    Tools

--- source/mobile/backend/StorageSystem.hx
+++ source/mobile/backend/StorageSystem.hx
@@ -15,6 +15,7 @@ import lime.app.Application;
 import haxe.io.Path;
 import haxe.io.Bytes;
 import openfl.utils.ByteArray;
+
 using StringTools;
 
 /** 
@@ -68,9 +69,9 @@ class StorageSystem
             }
 		}
 
-        /*if (!Permissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')) {
+        if (!Permissions.getGrantedPermissions().contains(['android.permission.READ_EXTERNAL_STORAGE', 'android.permission.WRITE_EXTERNAL_STORAGE'])) {
             Tools.showAlertDialog("Requires permissions", "Please allow the necessary permissions to play.\nPress OK & let's see what happens", {name: "OK", func: null}, null);
-		}*/
+		}
         #else
         trace("Permissions request not required or not implemented for this platform.");
         #end
